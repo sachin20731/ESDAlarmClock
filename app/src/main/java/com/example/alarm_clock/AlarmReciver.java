13e3b3085bbb6393db43df68f0ad0b95d73a9ce8
USER <Gihan8113>
2023-06-15 20:26:04 +0530

    create alarmReciver class

---
+++ app/src/main/java/com/example/alarm_clock/AlarmReciver.java
@@ -0,0 +1,23 @@
+package com.example.alarm_clock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    // implement onReceive() method
+    public void onReceive(Context context, Intent intent) {
+
+        // we will use vibrator first
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(4000);
